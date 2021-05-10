2b453bcd49e4f1bc4ff2366af25a907d82fbe00e
yuvanthb <82566595+yuvanthb@users.noreply.github.com>
2021-05-10 14:49:38 +0530

    Add files via upload

---
+++ js/step3.js
@@ -0,0 +1,59 @@
+
+    const URL = "https://teachablemachine.withgoogle.com/models/IinkddFKn/";
+
+    let model, webcam, labelContainer, maxPredictions;
+
+    // Load the image model and setup the webcam
+    async function init() {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // or files from your local hard drive
+        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const flip = true; // whether to flip the webcam
+        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append elements to the DOM
+        document.getElementById("webcam-container").appendChild(webcam.canvas);
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
+    }
+
+    async function loop() {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+    // run the webcam image through the image model
+    async function predict() {
+        // predict can take in an image, video or canvas html element
+        const prediction = await model.predict(webcam.canvas);
+        for (let i = 0; i < maxPredictions; i++) {
+            const classPrediction =
+                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            labelContainer.childNodes[i].innerHTML = classPrediction;
+            if(prediction[0].probability.toFixed(2) == 1.00){
+                if(confirm((`Marvelous! ${localStorage.getItem("kid_name")}. If you wash your hands all the seven steps, you will get a reward for killing the germs, Click OK to go to the next step`))){
+                    location.replace("step4.html")
+                    break
+                }
+                else{
+                    alert("Let us fight with COVID-19, Why not to finsh handwsahing completly? If you wash your hands all the seven steps, you will get a reward for killing the germs! ")
+                    location.replace("step4.html")
+                    break
+                } 
+            }
+        }
+    }
